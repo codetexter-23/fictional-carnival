121170d66e19532629e38cb30b00c94ce47fb326
codetexter-23 <138219728+codetexter-23@users.noreply.github.com>
2023-06-30 21:26:52 +0200

    Create first.js
    This is just a little code, checking two numbers if one of them is 100 or if their sum is 100 and returns true and if not returns false.

---
+++ first.js
@@ -0,0 +1,18 @@
+
+// in this exercise, I'll  write a program that checks if two numbers are 100 or the sum of the two numbers are 100 and return true or false if not.
+
+function (a,b){
+let sum=a+b;
+  if(a==100||b==100||sum==100){
+    return true;
+  }
+
+  else{
+   return false;
+
+  }
+
+}
+
+console.log(23,77);
+
