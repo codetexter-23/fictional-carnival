4cb2517c46af7b1f88a2084b994e68af4652658a
codetexter-23 <138219728+codetexter-23@users.noreply.github.com>
2023-06-30 21:34:37 +0200

    Update first.js

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 
 // in this exercise, I'll  write a program that checks if two numbers are 100 or the sum of the two numbers are 100 and return true or false if not.
 
-function (a,b){
+function  hundred(a,b){
 let sum=a+b;
   if(a==100||b==100||sum==100){
     return true;
@@ -14,5 +14,5 @@ let sum=a+b;
 
 }
 
-console.log(23,77);
+console.log(hundred(23,77));
 
