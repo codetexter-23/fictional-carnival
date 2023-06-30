59b9131895030e6c3e777bbb69a4102fea7190d6
codetexter-23 <138219728+codetexter-23@users.noreply.github.com>
2023-06-30 23:20:22 +0200

    Update first.js
    check if two numbers equal to 100 or if their sum is equal to 100 and return true and if not return false.

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 
-// in this exercise, I'll  write a program that checks if two numbers are 100 or the sum of the two numbers are 100 and return true or false if not.
+// in this exercise, I've  written a program that checks if two numbers are 100 or the sum of the two numbers are 100 and return true or false if not.
 
 function  hundred(a,b){
 let sum=a+b;
